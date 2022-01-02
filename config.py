1083fa5183ee4ed96c906c0801ac608064c91e24
Aykhan026 <78858146+aykhan026@users.noreply.github.com>
2022-01-03 00:53:41 +0400

    update config.py

--- config.py
+++ config.py
@@ -21,7 +21,9 @@ from telethon import TelegramClient, events
 api_id = int(os.environ.get("APP_ID"))
 api_hash = os.environ.get("API_HASH")
 bot_token = os.environ.get("TOKEN")
+
 client = TelegramClient('client', api_id, api_hash).start(bot_token=bot_token)
+
 admin_qrup = int(os.environ.get("ADMIN_QRUP"))
 etiraf_qrup = int(os.environ.get("ETIRAF_QRUP"))
 kanal = os.environ.get("kanal")
