fb4fa8130798a9365bacde817843cfe8d0a9c923
Aykhan026 <78858146+aykhan026@users.noreply.github.com>
2022-01-02 18:32:33 +0400

    Update etiraf.py

--- etiraf.py
+++ etiraf.py
@@ -27,7 +27,7 @@ from telethon import TelegramClient, events
 from telethon.tl.functions.messages import ForwardMessagesRequest
 from telethon.sessions import StringSession
 from telethon.tl.types import ChannelParticipantsAdmins
-from config import client, admin_qrup, etiraf_qrup, kanal, log_qrup, etirafmsg, startmesaj, qrupstart, botad, qrupstart, gonderildi, etirafyaz
+from config import client, admin_qrup, etiraf_qrup, kanal, log_qrup, etirafmsg, startmesaj, qrupstart, botad, qrupstart, gonderildi, etirafyaz, sahib, support
 
 logging.basicConfig(
     level=logging.INFO,
